b2b8b38f8dcfd45ab7b30a31738a588c4df35b94
Agnihotra_Nath <agnihotranath@hotmail.com>
2021-07-14 10:34:25 +0530

    Added Flyout for Controls

--- helloworld2/MainPage.xaml.vb
+++ helloworld2/MainPage.xaml.vb
@@ -13,4 +13,8 @@ Public NotInheritable Class MainPage
     Private Sub CommandBar_Opened(sender As Object, e As Object)
 
     End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+
+    End Sub
 End Class
